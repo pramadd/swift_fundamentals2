3b1c432a5a7a7898c8ad1089a336acdccaa307d7
Pranathi <neethu@codingdojo.com>
2017-11-05 20:13:47 -0800

    swift assignments basics

---
+++ Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 1...255 {
+    print(i)
+}
+
+for i in 1...100 {
+
+if i % 5 == 0 || i % 3 == 0 {
+    print(i)
+}
+}
+
+
+
+for i in 1...100{
+    if i % 5 == 0 && i % 3 == 0 {
+        print("fizzbuzz")
+        
+    }
+    else if i % 3 == 0 {
+        print("fizz")
+        
+    }
+    else if i % 5 == 0 {
+        print("buzz")
+        
+    }
+}
+
+
+
+
+
